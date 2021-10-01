4e6327fc247f7d3b16a65a24b627018a084a1d51
Fateme Zamanian <78360814+FatemeZamanian@users.noreply.github.com>
2021-10-01 12:46:11 +0330

    Update sheikhs_bot.py

--- sheikhs_bot.py
+++ sheikhs_bot.py
@@ -1,8 +1,7 @@
-from keras.models import load_model
+# from keras.models import load_model
 import telebot
-from telebot import types
-import numpy
-import cv2
+# import numpy
+# import cv2
 import os
 
 token = os.environ["token"]
@@ -10,23 +9,19 @@ bot = telebot.TeleBot(token)
 
 # model=load_model('sheikhs.h5')
 
-# btns=telebot.types.ReplyKeyboardMarkup(row_width=1)
-# btn1=telebot.types.KeyboardButton('Start')
-# btns.add(btn1)
 @bot.message_handler(commands=['start'])
 def wlc(message):
     bot.send_message(message.chat.id,'Hi 👋🏻 please send me a picture👀',
-    #reply_markup=btns
      )
 
-@bot.message_handler(content_types=['photo'])
-def photo(message):
-    fileID = message.photo[-1].file_id
-    file_info = bot.get_file(fileID)
-    src=file_info.file_path
-    downloaded_file = bot.download_file(src)
-    with open(src, 'wb') as new_file:
-        new_file.write(downloaded_file)
+# @bot.message_handler(content_types=['photo'])
+# def photo(message):
+#     fileID = message.photo[-1].file_id
+#     file_info = bot.get_file(fileID)
+#     src=file_info.file_path
+#     downloaded_file = bot.download_file(src)
+#     with open(src, 'wb') as new_file:
+#         new_file.write(downloaded_file)
 
 #     img_org = cv2.imread(src)
 #     img_RGB = cv2.cvtColor(img_org, cv2.COLOR_BGR2RGB)
