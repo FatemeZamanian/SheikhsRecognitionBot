acdeb18f3b52cc930955922bf068c9399e92e3e7
Fateme Zamanian <78360814+FatemeZamanian@users.noreply.github.com>
2021-10-01 12:56:47 +0330

    Update sheikhs_bot.py

--- sheikhs_bot.py
+++ sheikhs_bot.py
@@ -1,43 +1,42 @@
-# from keras.models import load_model
+from keras.models import load_model
 import telebot
-# import numpy
-# import cv2
+import numpy
+import cv2
 import os
 
 token = os.environ["token"]
 bot = telebot.TeleBot(token)
 
-# model=load_model('sheikhs.h5')
+model=load_model('sheikhs.h5')
 
 @bot.message_handler(commands=['start'])
 def wlc(message):
     bot.send_message(message.chat.id,'Hi 👋🏻 please send me a picture👀',
      )
 
-# @bot.message_handler(content_types=['photo'])
-# def photo(message):
-#     fileID = message.photo[-1].file_id
-#     file_info = bot.get_file(fileID)
-#     src=file_info.file_path
-#     downloaded_file = bot.download_file(src)
-#     with open(src, 'wb') as new_file:
-#         new_file.write(downloaded_file)
+@bot.message_handler(content_types=['photo'])
+def send_photo(message):
+    file_info = bot.get_file(message.photo[-1].file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    src = os.path.join('input', file_info.file_path)
+    with open(src, 'wb') as new_file:
+        new_file.write(downloaded_file)
 
-#     img_org = cv2.imread(src)
-#     img_RGB = cv2.cvtColor(img_org, cv2.COLOR_BGR2RGB)
-#     img_resize = cv2.resize(img_RGB, (224, 224))
+    img_org = cv2.imread(src)
+    img_RGB = cv2.cvtColor(img_org, cv2.COLOR_BGR2RGB)
+    img_resize = cv2.resize(img_RGB, (224, 224))
     
-#     img_numpy = numpy.array(img_resize)
-#     img = img_numpy / 255.0
-#     final = img.reshape(1, 224, 224, 3)
+    img_numpy = numpy.array(img_resize)
+    img = img_numpy / 255.0
+    final = img.reshape(1, 224, 224, 3)
 
-#     y_pred = numpy.argmax(model.predict(final))
+    y_pred = numpy.argmax(model.predict(final))
 
-#     if y_pred == 0:
-#         bot.reply_to(message,'normal person')
+    if y_pred == 0:
+        bot.reply_to(message,'normal person')
       
-#     else:
-#         bot.reply_to(message,'sheikh')
+    else:
+        bot.reply_to(message,'sheikh')
       
 
 bot.polling()
